5f57bea2e57dc75d0dd3c23c5039cbef90b4eed0
jgrabenstein <joe.grabenstein@incapturetechnologies.com>
2016-04-13 10:58:55 -0700

    UNFILED: get more debugging

--- Libs/test.sh
+++ Libs/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-{ 
-  ./gradlew test 
-} || { 
-  cat RaptureCore/build/reports/tests/index.html 
+{
+  ./gradlew test
+} || {
+  cat RaptureCore/build/reports/tests/index.html
+  cat RaptureCore/build/reports/tests/classes/rapture.dp.NestedSplitStepTest.html
+  cat RaptureCore/build/reports/tests/classes/rapture.dp.SimpleForkStepTest.html
+  cat RaptureCore/build/reports/tests/classes/rapture.dp.MemoryIndexHandlerTest.html
 }
-
