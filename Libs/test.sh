47043bf4df3f74c99e6e3af09438c194c45458d3
scarabus <dave.tong@incapturetechnologies.com>
2016-04-27 13:04:02 -0700

    Try to recover reports

--- Libs/test.sh
+++ Libs/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 {
-  env && ./gradlew test
+  ./gradlew test
 } || {
-  cat RaptureCore/build/reports/tests/index.html &&\
-  cat RaptureCore/build/reports/tests/classes/rapture.dp.NestedSplitStepTest.html &&\
-  cat RaptureCore/build/reports/tests/classes/rapture.dp.SimpleForkStepTest.html &&\
-  cat RaptureCore/build/reports/tests/classes/rapture.dp.MemoryIndexHandlerTest.html
+  (echo Directory is `pwd`;
+  find .. -name reports;
+  zip -r - `find .. -name reports` | uuencode - 2>/dev/null;
+  echo "======";)
 }
