e3d3a701bbad2b293e3cf19f95855f32b3f1e278
dukenguyen <duke.nguyen@incapturetechnologies.com>
2016-05-26 13:55:18 -0700

    RAP-4119 - add publishSlate task for generating slate docs

---
+++ Libs/CodeGen/ApiGen/SlatePublish/config.rb
@@ -0,0 +1,36 @@
+# Markdown
+
+# i set this for jruby ...
+#set :markdown_engine, :kramdown
+set :markdown_engine, :redcarpet
+set :markdown,
+    fenced_code_blocks: true,
+    smartypants: true,
+    disable_indented_code_blocks: true,
+    prettify: true,
+    tables: true,
+    with_toc_data: true,
+    no_intra_emphasis: true
+
+# Assets
+set :css_dir, 'stylesheets'
+set :js_dir,  'javascripts'
+set :images_dir, 'images'
+set :fonts_dir, 'fonts'
+set :source,  'source'
+
+# Activate the syntax highlighter
+activate :syntax
+
+# Github pages require relative links
+activate :relative_assets
+set :relative_links, true
+
+# Build Configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  # activate :relative_assets
+  # activate :asset_hash
+  # activate :gzip
+end
