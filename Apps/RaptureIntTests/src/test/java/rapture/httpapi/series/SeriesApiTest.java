2d0923c900d950be8dc131e558175dbf2b8df15a
jeremy-flieder <jeremyflieder@Jeremys-MacBook-Pro.local>
2016-06-06 13:31:18 -0700

    RAP-4140: add series test for delete and update and improve results checking for other tests

--- Apps/RaptureIntTests/src/test/java/rapture/httpapi/series/SeriesApiTest.java
+++ Apps/RaptureIntTests/src/test/java/rapture/httpapi/series/SeriesApiTest.java
@@ -1,7 +1,11 @@
 package rapture.httpapi.series;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.testng.Assert;
 import org.testng.Reporter;
@@ -14,7 +18,6 @@ import org.testng.annotations.Test;
 import rapture.common.RaptureURI;
 import rapture.common.Scheme;
 import rapture.common.SeriesDouble;
-import rapture.common.SeriesRepoConfig;
 import rapture.common.SeriesString;
 import rapture.common.client.HttpSeriesApi;
 import rapture.common.client.HttpLoginApi;
@@ -60,16 +63,20 @@ public class SeriesApiTest {
             pointKeys.add(new Integer (i).toString());
             pointValues.add("testValue"+i);
         }
-        String newSeries=repoName+"/addStrings"+System.nanoTime();
+        String newSeries=repoName+"addStrings"+System.nanoTime();
         Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
         seriesApi.addStringsToSeries(newSeries, pointKeys, pointValues);
         Reporter.log("Checking points in "+newSeries, true);
         List <SeriesString> seriesList=seriesApi.getPointsAsStrings(newSeries);
         Assert.assertTrue(seriesList.size() > 0);
+        Set<String> keySet = new HashSet<String>();
+        Set<String> valueSet = new HashSet<String>();
         for (SeriesString s : seriesList) {
-            Assert.assertTrue(pointKeys.contains(s.getKey()));
-            Assert.assertTrue(pointValues.contains(s.getValue()));
-        }         
+            keySet.add(s.getKey());
+            valueSet.add(s.getValue());
+        } 
+        Assert.assertEquals(keySet, new HashSet<String>(pointKeys));
+        Assert.assertEquals(valueSet, new HashSet<String>(pointValues));
     }
     
     @Test (groups={"series","cassandra", "nightly"})
@@ -84,16 +91,20 @@ public class SeriesApiTest {
             pointKeys.add(new Integer (i).toString());
             pointValues.add(new Long(i));
         }
-        String newSeries=repoName+"/addLongs"+System.nanoTime();
+        String newSeries=repoName+"addLongs"+System.nanoTime();
         Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
         seriesApi.addLongsToSeries(newSeries, pointKeys, pointValues);
         Reporter.log("Checking points in "+newSeries, true);
         List <SeriesDouble> seriesList=seriesApi.getPointsAsDoubles(newSeries);
         Assert.assertTrue(seriesList.size() > 0);
+        Set<String> keySet = new HashSet<String>();
+        Set<Long> valueSet = new HashSet<Long>();
         for (SeriesDouble s : seriesList) {
-            Assert.assertTrue(pointKeys.contains(s.getKey()));
-            Assert.assertTrue(pointValues.contains(s.getValue().longValue()));
-        }         
+            keySet.add(s.getKey());
+            valueSet.add(new Long(s.getValue().longValue()));
+        } 
+        Assert.assertEquals(keySet, new HashSet<String>(pointKeys));
+        Assert.assertEquals(valueSet, new HashSet<Long>(pointValues));       
     }
     
     @Test (groups={"series","cassandra", "nightly"})
@@ -108,16 +119,97 @@ public class SeriesApiTest {
             pointKeys.add(new Integer (i).toString());
             pointValues.add(new Double(i));
         }
-        String newSeries=repoName+"/addDoubles"+System.nanoTime();
+        String newSeries=repoName+"addDoubles"+System.nanoTime();
         Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
         seriesApi.addDoublesToSeries(newSeries, pointKeys, pointValues);
         Reporter.log("Checking points in "+newSeries, true);
         List <SeriesDouble> seriesList=seriesApi.getPointsAsDoubles(newSeries);
-        Assert.assertTrue(seriesList.size() > 0);
+        Set<String> keySet = new HashSet<String>();
+        Set<Double> valueSet = new HashSet<Double>();
         for (SeriesDouble s : seriesList) {
-            Assert.assertTrue(pointKeys.contains(s.getKey()));
-            Assert.assertTrue(pointValues.contains(s.getValue().doubleValue()));
-        }         
+            keySet.add(s.getKey());
+            valueSet.add(s.getValue());
+        } 
+        Assert.assertEquals(keySet, new HashSet<String>(pointKeys));
+        Assert.assertEquals(valueSet, new HashSet<Double>(pointValues));     
+    }
+    
+    @Test (groups={"series","cassandra", "nightly"})
+    public void testAddDoublesToSeriesAndUpdate () {
+        int MAX_VALUES=100;
+        RaptureURI repo = helper.getRandomAuthority(Scheme.SERIES);
+        helper.configureTestRepo(repo, "CASSANDRA");
+        String repoName= new RaptureURI.Builder(repo).docPath("").build().toString();
+        List <String> pointKeys = new ArrayList <String>();
+        List <Double> pointValues = new ArrayList <Double>();
+        for (int i = 0; i < MAX_VALUES;i++) {
+            pointKeys.add(new Integer (i).toString());
+            pointValues.add(new Double(i));
+        }
+        String newSeries=repoName+"updateDoubles"+System.nanoTime();
+        Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
+        seriesApi.addDoublesToSeries(newSeries, pointKeys, pointValues);
+        Reporter.log("Checking points in "+newSeries, true);
+        List <SeriesDouble> seriesList=seriesApi.getPointsAsDoubles(newSeries);
+        Set<String> keySet = new HashSet<String>();
+        Set<Double> valueSet = new HashSet<Double>();
+        for (SeriesDouble s : seriesList) {
+            keySet.add(s.getKey());
+            valueSet.add(s.getValue());
+        } 
+        Assert.assertEquals(keySet, new HashSet<String>(pointKeys));
+        Assert.assertEquals(valueSet, new HashSet<Double>(pointValues));     
+        
+        Reporter.log("Updating "+pointKeys.size() + " points to "+newSeries, true);
+        Map<String, Double> checkMap = new HashMap<String,Double>();
+        Reporter.log("Checking points in "+newSeries, true);
+        for (int i = 0; i < MAX_VALUES;i++) {
+        	seriesApi.addDoubleToSeries(newSeries, new Integer (i).toString(), new Double(i+MAX_VALUES));
+        	checkMap.put(new Integer (i).toString(),  new Double(i+MAX_VALUES));
+        }
+        seriesList=seriesApi.getPointsAsDoubles(newSeries);
+        for (SeriesDouble s : seriesList) {
+        	Assert.assertEquals(s.getValue(),checkMap.get(s.getKey()));
+        } 
+    }
+    
+    @Test (groups={"series","cassandra", "nightly"})
+    public void testAddStringsToSeriesAndUpdate () {
+        int MAX_VALUES=100;
+        RaptureURI repo = helper.getRandomAuthority(Scheme.SERIES);
+        helper.configureTestRepo(repo, "CASSANDRA");
+        String repoName= new RaptureURI.Builder(repo).docPath("").build().toString();
+        List <String> pointKeys = new ArrayList <String>();
+        List <String> pointValues = new ArrayList <String>();
+        for (int i = 0; i < MAX_VALUES;i++) {
+            pointKeys.add(new Integer (i).toString());
+            pointValues.add("point"+i);
+        }
+        String newSeries=repoName+"updateStrings"+System.nanoTime();
+        Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
+        seriesApi.addStringsToSeries(newSeries, pointKeys, pointValues);
+        Reporter.log("Checking points in "+newSeries, true);
+        List <SeriesString> seriesList=seriesApi.getPointsAsStrings(newSeries);
+        Set<String> keySet = new HashSet<String>();
+        Set<String> valueSet = new HashSet<String>();
+        for (SeriesString s : seriesList) {
+            keySet.add(s.getKey());
+            valueSet.add(s.getValue());
+        } 
+        Assert.assertEquals(keySet, new HashSet<String>(pointKeys));
+        Assert.assertEquals(valueSet, new HashSet<String>(pointValues));     
+        
+        Reporter.log("Updating "+pointKeys.size() + " points to "+newSeries, true);
+        Map<String, String> checkMap = new HashMap<String,String>();
+        Reporter.log("Checking points in "+newSeries, true);
+        for (int i = 0; i < MAX_VALUES;i++) {
+        	seriesApi.addStringToSeries(newSeries, new Integer (i).toString(), "point"+(i+MAX_VALUES));
+        	checkMap.put(new Integer (i).toString(), "point"+(i+MAX_VALUES));
+        }
+        seriesList=seriesApi.getPointsAsStrings(newSeries);
+        for (SeriesString s : seriesList) {
+        	Assert.assertEquals(s.getValue(),checkMap.get(s.getKey()));
+        } 
     }
     
     @Test (groups={"series","cassandra", "nightly"})
@@ -135,7 +227,7 @@ public class SeriesApiTest {
             pointKeys.add(new Integer (i).toString());
             pointValues.add(new Double(i));
         }
-        String newSeries=repoName+"/getDoublesRanges"+System.nanoTime();
+        String newSeries=repoName+"getDoublesRanges"+System.nanoTime();
         Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
         seriesApi.addDoublesToSeries(newSeries, pointKeys, pointValues);
         Reporter.log("Checking points in "+newSeries, true);
@@ -158,16 +250,63 @@ public class SeriesApiTest {
             pointKeys.add(new Integer (i).toString());
             pointValues.add(new Double(i));
         }
-        String newSeries=repoName+"/deleteSeriesByKey"+System.nanoTime();
+        String newSeries=repoName+"deleteSeriesByKey"+System.nanoTime();
         Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
         seriesApi.addDoublesToSeries(newSeries, pointKeys, pointValues);
-        
+        Reporter.log("Deleting "+(pointKeys.size()/2) + " points to "+newSeries, true);
         for (int i=0;i<MAX_VALUES/2;i++)
             pointKeys.remove(0);
         seriesApi.deletePointsFromSeriesByPointKey(newSeries, pointKeys);
-        for (SeriesDouble s : seriesApi.getPointsAsDoubles(newSeries)) {
+        Reporter.log("Checking "+newSeries, true);
+        List <SeriesDouble> seriesList=seriesApi.getPointsAsDoubles(newSeries);
+        for (SeriesDouble s : seriesList) {
             Assert.assertTrue(Integer.parseInt(s.getKey()) < (MAX_VALUES/2));
         } 
+        Reporter.log("Deleting remaining points to "+newSeries, true);
+        pointKeys = new ArrayList <String>();
+        for (SeriesDouble s : seriesList)
+        	pointKeys.add(s.getKey());
+        seriesApi.deletePointsFromSeriesByPointKey(newSeries, pointKeys);
+        Reporter.log("Checking "+newSeries+ " has size of 0", true);
+        Assert.assertEquals(seriesApi.getPointsAsDoubles(newSeries).size(), 0);   
+    }
+    
+    @Test (groups={"series","cassandra", "nightly"})
+    public void testDeleteAndUpdateSeries () {
+        int MAX_VALUES=200;
+        RaptureURI repo = helper.getRandomAuthority(Scheme.SERIES);
+        helper.configureTestRepo(repo, "CASSANDRA");
+        String repoName= new RaptureURI.Builder(repo).docPath("").build().toString();
+        List <String> pointKeys = new ArrayList <String>();
+        List <Double> pointValues = new ArrayList <Double>();
+        for (int i = 0; i < MAX_VALUES;i++) {
+            pointKeys.add(new Integer (i).toString());
+            pointValues.add(new Double(i));
+        }
+        String newSeries=repoName+"deleteUpdateSeries"+System.nanoTime();
+        Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
+        seriesApi.addDoublesToSeries(newSeries, pointKeys, pointValues);
+        Reporter.log("Deleting "+(pointKeys.size()/2) + " points to "+newSeries, true);
+        for (int i=0;i<MAX_VALUES/2;i++)
+            pointKeys.remove(0);
+        seriesApi.deletePointsFromSeriesByPointKey(newSeries, pointKeys);
+        Reporter.log("Checking "+newSeries, true);
+        List <SeriesDouble> seriesList=seriesApi.getPointsAsDoubles(newSeries);
+        for (SeriesDouble s : seriesList) {
+            Assert.assertTrue(Integer.parseInt(s.getKey()) < (MAX_VALUES/2));
+        } 
+        Reporter.log("Updating remaining points to "+newSeries, true);
+        Map <String, Double> updateMap=new HashMap <String,Double>();
+        for (SeriesDouble s : seriesList) {
+        	seriesApi.addDoubleToSeries(newSeries, s.getKey(), new Double(s.getValue().doubleValue()+MAX_VALUES));
+        	updateMap.put(s.getKey(), new Double(s.getValue().doubleValue()+MAX_VALUES));
+        }
+        Reporter.log("Check points in "+newSeries, true);
+        seriesList=seriesApi.getPointsAsDoubles(newSeries);
+        for (SeriesDouble s : seriesList) {
+            Assert.assertEquals(s.getValue(), updateMap.get(s.getKey()));
+        } 
+ 
     }
     
     @Test (groups={"series","cassandra", "nightly"})
@@ -182,13 +321,11 @@ public class SeriesApiTest {
             pointKeys.add(new Integer (i).toString());
             pointValues.add(new Double(i));
         }
-        String newSeries=repoName+"/deleteSeries"+System.nanoTime();
+        String newSeries=repoName+"deleteSeries"+System.nanoTime();
         Reporter.log("Adding "+pointKeys.size() + " points to "+newSeries, true);
         seriesApi.addDoublesToSeries(newSeries, pointKeys, pointValues);
         seriesApi.deletePointsFromSeries(newSeries);
-        
         Assert.assertEquals(seriesApi.getPoints(newSeries).size(),0);
-       
     }
     
     @AfterClass(groups={"series","cassandra", "nightly"})
