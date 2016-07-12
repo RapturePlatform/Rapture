b30f610e706dcef9a53a8d32736ad65e0ff867af
scarabus <dave.tong@incapturetechnologies.com>
2016-07-12 13:35:34 -0700

    add parent nesting

--- Apps/RaptureIntTests/src/test/python/operation.py
+++ Apps/RaptureIntTests/src/test/python/operation.py
@@ -59,14 +59,34 @@ def test_interfaceOperation():
     assert content['charlie'] == 4 
 
 def test_parentOperation():
+
+    parent1 = repo+"/parent1"
+    parent2 = repo+"/parent2"
+    parent3 = repo+"/parent3"
+
     # simple document with $parent
-    rapture.doDoc_PutDoc(docu, '{"alpha":1,"bravo":2,"charlie":3,"$parent":"'+oper+'"}')
+    rapture.doDoc_PutDoc(docu, '{"alpha":1,"bravo":2,"charlie":3,"$parent":"'+parent1+'"}')
 
     # Put the operations in $parent document
-    rapture.doDoc_PutDoc(oper, '{ "setVal" : "this[params[\'fieldToChange\']] = params[\'newFieldValue\']; return this;", "increment" : "this[params[\'fieldToIncrement\']] = cast(this[params[\'fieldToIncrement\']], \'integer\') + 1; println(this); return this;" }')
+    rapture.doDoc_PutDoc(parent1, '{ "setVal" : "this[\'alpha\'] = params[\'newFieldValue\']; return this;", "$parent" : "'+parent2+'" }')
+    rapture.doDoc_PutDoc(parent2, '{ "setVal" : "this[\'bravo\'] = params[\'newFieldValue\']; return this;", "$parent" : "'+parent3+'" }')
+    rapture.doDoc_PutDoc(parent3, '{ "increment" : "this[params[\'fieldToIncrement\']] = cast(this[params[\'fieldToIncrement\']], \'integer\') + 1; println(this); return this;"}');
 
-    m = {'fieldToChange':'bravo', 'newFieldValue': 'Five', 'fieldToIncrement': 'charlie'}
+    m = {'newFieldValue': 'Five', 'fieldToIncrement': 'charlie'}
     rapture.doOperation_InvokeSave(docu, 'setVal', m)
     content = rapture.doOperation_Invoke(docu, 'increment', m)
-    assert content['bravo'] == "Five" 
+    assert content['alpha'] == "Five" 
+    assert content['bravo'] == 2
     assert content['charlie'] == 4 
+
+    rapture.doDoc_PutDoc(docu, '{"alpha":1,"bravo":2,"charlie":3,"$parent":"'+oper+'", "$interface" : "'+parent2+'"}')
+    ## This will call setVal from parent2 because although parent1 is imported via the $PARENT clause
+    ## parent2 is imported via the $INTERFACE clause, which takes precedence.
+
+    m = {'newFieldValue': 'Five', 'fieldToIncrement': 'charlie'}
+    rapture.doOperation_InvokeSave(docu, 'setVal', m)
+    content = rapture.doOperation_Invoke(docu, 'increment', m)
+    assert content['alpha'] == 1
+    assert content['bravo'] == "Five"
+    assert content['charlie'] == 4 
+
