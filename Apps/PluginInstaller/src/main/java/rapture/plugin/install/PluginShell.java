78a9802b1f3a83a573885a5a5d47f358e5b1b595
scarabus <dave.tong@incapturetechnologies.com>
2016-07-21 15:35:42 -0700

    RAP-4199    NPE while uninstalling plugin

--- Apps/PluginInstaller/src/main/java/rapture/plugin/install/PluginShell.java
+++ Apps/PluginInstaller/src/main/java/rapture/plugin/install/PluginShell.java
@@ -33,6 +33,7 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.SortedSet;
 import java.util.regex.Matcher;
@@ -46,6 +47,11 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.log4j.Appender;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import rapture.common.PluginConfig;
 import rapture.common.PluginManifest;
@@ -79,11 +85,6 @@ import rapture.plugin.validators.ScriptValidator;
 import rapture.plugin.validators.SeriesValidator;
 import rapture.plugin.validators.SnippetValidator;
 import rapture.plugin.validators.WorkflowValidator;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 
 /**
  * This class implements a simple DSL for defining, uploading, downloading, installing, uninstalling, and upgrading collections of user content called "plugins"
@@ -229,6 +230,8 @@ public class PluginShell {
                 }
                 eval(line);
             }
+        } catch (NoSuchElementException nsee) {
+            System.out.println("Be seeing you");
         }
     }
 
