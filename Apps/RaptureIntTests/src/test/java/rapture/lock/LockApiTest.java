5ccfa79d02f4a2a674e27e38fabe3296d674eb5e
scarabus <dave.tong@incapturetechnologies.com>
2016-07-22 13:14:29 -0700

    Separate Mongo and Zookeeper integration tests

--- Apps/RaptureIntTests/src/test/java/rapture/lock/LockApiTest.java
+++ Apps/RaptureIntTests/src/test/java/rapture/lock/LockApiTest.java
@@ -85,13 +85,51 @@ public class LockApiTest {
     @AfterClass(groups = { "nightly" })
     public void tearDown() {
     }
-        
-    @Test(groups = { "nightly" }, enabled = true)
-    public void testLock() throws InterruptedException {
+
+    @Test(groups = { "nightly", "zookeeper" }, enabled = true)
+    public void testZookeeperLock() throws InterruptedException {
 
         // Player 1 acquires a lock
         RaptureURI lockUri = RaptureURI.builder(helper.getRandomAuthority(Scheme.DOCUMENT)).docPath("foo/bar" + System.currentTimeMillis()).build();
         RaptureLockConfig lockConfig = lockApi.createLockManager(lockUri.toString(), "LOCKING USING ZOOKEEPER {}", "");
+        assertNotNull(lockConfig);
+        LockHandle lockHandle = lockApi.acquireLock(lockUri.toString(), lockConfig.getName(), 1, 60);
+        assertNotNull(lockHandle);
+        Thread.sleep(100);
+
+        // Meanwhile elsewhere Player 2 tries to acquire the lock
+        RaptureLockConfig lockConfig2 = lockApi.getLockManagerConfig(lockUri.toString());
+        assertNotNull(lockConfig2);
+        LockHandle lockHandle2 = lockApi2.acquireLock(lockUri.toString(), lockConfig2.getName(), 1, 60);
+        // but fails
+        assertNull(lockHandle2);
+
+        // Eventually player1 releases the lock
+        Thread.sleep(100);
+        assertTrue(lockApi.releaseLock(lockUri.toString(), lockConfig.getName(), lockHandle));
+        assertFalse(lockApi2.releaseLock(lockUri.toString(), lockConfig2.getName(), lockHandle2));
+
+        // and now Player 2 can acquire it
+        lockHandle2 = lockApi2.acquireLock(lockUri.toString(), lockConfig2.getName(), 1, 60);
+        assertNotNull(lockHandle2);
+        assertTrue(lockApi2.releaseLock(lockUri.toString(), lockConfig2.getName(), lockHandle2));
+        assertFalse(lockApi2.releaseLock(lockUri.toString(), lockConfig2.getName(), lockHandle2));
+
+        assertNotNull(lockApi.getLockManagerConfig(lockUri.toString()));
+        assertTrue(lockApi.lockManagerExists(lockUri.toString()));
+        lockApi.deleteLockManager(lockUri.toString());
+        assertFalse(lockApi.lockManagerExists(lockUri.toString()));
+        assertNull(lockApi.getLockManagerConfig(lockUri.toString()));
+        System.out.println(lockUri.toString());
+
+    }
+
+    @Test(groups = { "nightly", "mongodb" }, enabled = true)
+    public void testMongoDBLock() throws InterruptedException {
+
+        // Player 1 acquires a lock
+        RaptureURI lockUri = RaptureURI.builder(helper.getRandomAuthority(Scheme.DOCUMENT)).docPath("foo/bar" + System.currentTimeMillis()).build();
+        RaptureLockConfig lockConfig = lockApi.createLockManager(lockUri.toString(), "LOCKING USING MONGODB {}", "");
         assertNotNull(lockConfig);
         LockHandle lockHandle = lockApi.acquireLock(lockUri.toString(), lockConfig.getName(), 1, 60);
         assertNotNull(lockHandle);
