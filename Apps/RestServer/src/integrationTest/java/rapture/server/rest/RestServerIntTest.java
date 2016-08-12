6096934a8aced1596b69d228bde0e753b656c589
dukenguyen <duke.nguyen@incapturetechnologies.com>
2016-08-12 11:10:25 -0700

    RAP-4227 - basic auth and https

--- Apps/RestServer/src/integrationTest/java/rapture/server/rest/RestServerIntTest.java
+++ Apps/RestServer/src/integrationTest/java/rapture/server/rest/RestServerIntTest.java
@@ -31,6 +31,15 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -40,16 +49,11 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class RestServerIntTest {
 
-    private void printResponse(HttpResponse<String> result) {
-        System.out.println(result.getBody());
-        System.out.println(result.getStatus() + ": " + result.getStatusText());
-    }
-
     @Before
     public void setup() throws UnirestException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-        HttpResponse<String> result = Unirest.post("http://localhost:4567/login")
-                .body("{\"username\":\"rapture\", \"password\":\"rapture\"}")
-                .asString();
+        setupHttpsUnirest();
+        HttpResponse<String> result = Unirest.post("https://localhost:4567/login")
+                .body("{\"username\":\"rapture\", \"password\":\"rapture\"}").asString();
         printResponse(result);
         if (result.getStatus() != 200) {
             fail("Failed to login");
@@ -57,49 +61,78 @@ public class RestServerIntTest {
     }
 
     @Test
+    public void testBadLogin() throws UnirestException {
+        HttpResponse<String> result = Unirest.post("https://localhost:4567/login")
+                .body("{\"username\":\"rapture\", \"password\":\"sadfasdf\"}").asString();
+        printResponse(result);
+        assertEquals(401, result.getStatus());
+    }
+
+    @Test
     public void testDocGet() throws UnirestException {
-        HttpResponse<String> result = Unirest.get("http://localhost:4567/doc/matrix.config/mqSeriesConfig").asString();
+        HttpResponse<String> result = Unirest.get("https://localhost:4567/doc/config/mqSeriesConfig").asString();
         printResponse(result);
         assertTrue(result.getBody().indexOf("connectionFactory") != -1);
     }
 
     @Test
     public void testDocPut() throws UnirestException {
-        int status = Unirest.put("http://localhost:4567/doc/matrix.config/mqSeriesConfig")
-                .body("{ \"environment\" : \"test\", \"connectionFactory\" : \"MATRIXCF\", \"outputQueue\" : \"RAP_TO_TB\", \"writeEnabled\" : false }")
-                .asString().getStatus();
+        int status = Unirest.put("https://localhost:4567/doc/config/mqSeriesConfig")
+                .body("{ \"environment\" : \"test\", \"connectionFactory\" : \"CF\", \"outputQueue\" : \"RAP_TO_TB\", \"writeEnabled\" : false }").asString()
+                .getStatus();
         assertEquals(200, status);
     }
 
     @Test
     public void testDocDelete() throws UnirestException {
-        String result = Unirest.delete("http://localhost:4567/doc/matrix.config/mqSeriesConfig").asString().getBody();
-        System.out.println(result);
-        int status = Unirest.get("http://localhost:4567/doc/matrix.config/mqSeriesConfig").asString().getStatus();
+        HttpResponse<String> result = Unirest.delete("https://localhost:4567/doc/config/mqSeriesConfig").asString();
+        printResponse(result);
+        int status = Unirest.get("https://localhost:4567/doc/config/mqSeriesConfig").asString().getStatus();
         assertEquals(404, status);
     }
 
     @Test
     public void testBlobGet() throws UnirestException {
-        String result = Unirest.get("http://localhost:4567/blob/matrix.archive/FIX/2016/05/18/8668_input").asString().getBody();
-        System.out.println(result);
+        HttpResponse<String> result = Unirest.get("https://localhost:4567/blob/archive/FIX/2016/05/18/8668_input").asString();
+        printResponse(result);
     }
 
     @Test
     public void testBlobPut() throws UnirestException {
-        int status = Unirest.put("http://localhost:4567/blob/matrix.archive/FIX/2016/05/18/8668_input")
-                .header("Content-Type", "text/plain")
-                .body("a string value")
+        int status = Unirest.put("https://localhost:4567/blob/archive/FIX/2016/05/18/8668_input").header("Content-Type", "text/plain").body("a string value")
                 .asString().getStatus();
         assertEquals(200, status);
     }
 
     @Test
     public void testBlobDelete() throws UnirestException {
-        String result = Unirest.delete("http://localhost:4567/blob/matrix.archive/FIX/2016/05/18/8668_input").asString().getBody();
-        System.out.println(result);
-        int status = Unirest.get("http://localhost:4567/blob/matrix.archive/FIX/2016/05/18/8668_input").asString().getStatus();
+        HttpResponse<String> result = Unirest.delete("https://localhost:4567/blob/archive/FIX/2016/05/18/8668_input").asString();
+        printResponse(result);
+        int status = Unirest.get("https://localhost:4567/blob/archive/FIX/2016/05/18/8668_input").asString().getStatus();
         assertEquals(404, status);
+    }
+
+    private void printResponse(HttpResponse<String> result) {
+        System.out.println(result.getBody());
+        System.out.println(result.getStatus() + ": " + result.getStatusText());
+    }
+
+    private void setupHttpsUnirest() {
+        SSLContext sslcontext = null;
+        try {
+            sslcontext = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+            fail(e.toString());
+        }
+
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        Unirest.setHttpClient(httpclient);
     }
 
 }
