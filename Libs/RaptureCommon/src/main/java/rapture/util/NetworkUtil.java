7a21decc9aea07f5eae3bff6e4cb2f251afd2938
Jonathan Major <jonathan.major@incapturetechnologies.com>
2016-10-05 11:13:15 -0700

    reverting change and ignoring test

--- Libs/RaptureCommon/src/main/java/rapture/util/NetworkUtil.java
+++ Libs/RaptureCommon/src/main/java/rapture/util/NetworkUtil.java
@@ -77,7 +77,7 @@ public class NetworkUtil {
             while (addrs.hasMoreElements()) {
                 InetAddress addr = addrs.nextElement();
                 log.debug("IP addr is: " + addr.getHostAddress());
-                if (addr.isSiteLocalAddress() || (addr.isLoopbackAddress())) {
+                if (addr.isSiteLocalAddress()) {
                     return addr.getHostAddress();
                 }
             }
