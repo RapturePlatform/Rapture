ecbc6da48f3599307bb894ca3f9e4e2e93db6756
scarabus <dave.tong@incapturetechnologies.com>
2016-10-12 15:11:39 -0700

    MAT-101 Workorder URI not showing up in slack message from scheduled jobs

--- Libs/RaptureCore/src/main/java/rapture/kernel/DecisionApiImpl.java
+++ Libs/RaptureCore/src/main/java/rapture/kernel/DecisionApiImpl.java
@@ -411,20 +411,22 @@ public class DecisionApiImpl extends KernelBase implements DecisionApi {
             ret.setIsCreated(true);
             ret.setUri(order);
 
-            String docPath = workOrderURI.getDocPath();
-            int lio = docPath.lastIndexOf('/');
-
-            StringBuilder externalUrl = new StringBuilder();
-            String host = System.getenv("HOST");
-            String port = System.getenv("PORT");
-            externalUrl.append("http://").append((host != null) ? host : "localhost").append(":").append((port != null) ? port : "8000").append("/process/").append(docPath.substring(0, lio))
-                    .append(WORKORDER_DELIMETER).append(docPath.substring(lio + 1));
-            // deprecated
-            this.setContextLiteral(context, order, "WORKFLOWURL", externalUrl.toString());
-            this.setContextLiteral(context, order, "WORKORDERURL", externalUrl.toString());
+            setWorkOrderUri(order);
 
             return ret;
         }
+    }
+
+    public void setWorkOrderUri(String workOrder) {
+        String docPath = new RaptureURI(workOrder).getDocPath();
+        int lio = docPath.lastIndexOf('/');
+
+        StringBuilder externalUrl = new StringBuilder();
+        String host = System.getenv("HOST");
+        String port = System.getenv("PORT");
+        externalUrl.append("http://").append((host != null) ? host : "localhost").append(":").append((port != null) ? port : "8000").append("/process/")
+                .append(docPath.substring(0, lio)).append(WORKORDER_DELIMETER).append(docPath.substring(lio + 1));
+        setContextLiteral(ContextFactory.getKernelUser(), workOrder, "WORKORDERURL", externalUrl.toString());
     }
 
     private static void writeWorkOrderArguments(CallingContext context, Map<String, String> argsMap, WorkOrder workOrder) {
