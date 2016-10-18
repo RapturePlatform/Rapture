2fa42e981a4533583a010395a8819c6b7238186e
scarabus <dave.tong@incapturetechnologies.com>
2016-10-17 17:38:41 -0700

    MAT-106

--- Libs/WorkflowsCommonSteps/src/test/java/rapture/dp/invocable/ftp/steps/CheckFileExistsStepTest.java
+++ Libs/WorkflowsCommonSteps/src/test/java/rapture/dp/invocable/ftp/steps/CheckFileExistsStepTest.java
@@ -2,17 +2,43 @@ package rapture.dp.invocable.ftp.steps;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
 
 import rapture.common.CallingContext;
+import rapture.common.RaptureConstants;
+import rapture.common.RaptureURI;
+import rapture.common.Scheme;
 import rapture.common.impl.jackson.JacksonUtil;
+import rapture.kernel.AuditApiImpl;
 import rapture.kernel.ContextFactory;
 import rapture.kernel.Kernel;
 import rapture.kernel.dp.ExecutionContextUtil;
 
 public class CheckFileExistsStepTest {
+
+    AuditApiImpl audit;
+
+    @Before
+    public void doBefore() {
+        System.setProperty("LOGSTASH-ISENABLED", "false");
+        audit = Kernel.getAudit().getTrusted();
+        CallingContext context = ContextFactory.getKernelUser();
+        if (audit.doesAuditLogExist(context, RaptureConstants.DEFAULT_AUDIT_URI)) {
+            audit.deleteAuditLog(context, RaptureConstants.DEFAULT_AUDIT_URI);
+
+        }
+        if (!audit.doesAuditLogExist(context, RaptureConstants.DEFAULT_AUDIT_URI)) {
+            audit.createAuditLog(context, new RaptureURI(RaptureConstants.DEFAULT_AUDIT_URI, Scheme.LOG).getAuthority(), "LOG {} using MEMORY {}");
+        }
+
+        if (!audit.doesAuditLogExist(context, "workflow")) {
+            audit.createAuditLog(context, "workflow", "LOG {} using MEMORY {}");
+        }
+
+    }
 
     @SuppressWarnings({ "unused", "static-access" })
     @Test
@@ -30,5 +56,18 @@ public class CheckFileExistsStepTest {
         Kernel.getDecision().setContextLiteral(ctx, workerUri, "EXIST_FILENAMES", JacksonUtil.jsonFromObject(ImmutableMap.of(filename, true)));
         assertEquals(cfes.getFailTransition(), cfes.invoke(ctx));
         assertEquals("/tmp/19780606 was not found but was expected ", Kernel.getDecision().getContextValue(ctx, workerUri, "fooError"));
+    }
+
+    @Test
+    public void checkWildcardTest() {
+        CallingContext ctx = ContextFactory.getAnonymousUser();
+        String workerUri = "workorder://x/y#0";
+        String workOrderUri = workerUri.substring(0, workerUri.length() - 2);
+        CheckFileExistsStep cfes = new CheckFileExistsStep(workerUri, "foo");
+        String filename = "/etc/aliase.";
+        String expand = ExecutionContextUtil.evalTemplateECF(ctx, workerUri, filename, null);
+        Kernel.getDecision().setContextLiteral(ctx, workerUri, "EXIST_FILENAMES", JacksonUtil.jsonFromObject(ImmutableMap.of("/bin/c.", Boolean.TRUE,
+                "file://bin/m[a-z]{4}", Boolean.TRUE, "file://.{3}/zsh", Boolean.TRUE, "file://x.*/y.*/z.*", Boolean.FALSE, "/b{7}/w.*/f?/c", Boolean.FALSE)));
+        assertEquals(cfes.getNextTransition(), cfes.invoke(ctx));
     }
 }
